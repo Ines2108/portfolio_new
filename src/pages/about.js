83cc295507c904a05997df9227adb73ba6b12ade
inesniedermaier <ines.niedermaier@gmail.com>
2025-06-25 13:41:45 +0200

    Adjust padding in polaroidImages

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,7 +27,7 @@ const about = () => {
           <AnimatedHeadline text="Das bin ich" className="text-primary" />
           {/* About Section */}
           <section className="about-section relative max-w-[1800px] w-full mx-auto flex flex-col-reverse lg:flex-row items-center lg:items-start mt-10 mb-36 md:mb-16 lg:mb-32">
-            <div className="w-full h-full self-center lg:w-1/2 flex justify-center lg:justify-start pl-0 xl:pl-16 xxl:pl-36 pt-4 lg:pt-28">
+            <div className="w-full h-full self-center lg:w-1/2 flex justify-center lg:justify-start pl-0 xl:pl-16 xxl:pl-36 pt-4 lg:pt-12">
               <PolaroidImages />
             </div>
             <div className="w-full lg:w-1/2 px-0 lg:pl-10 xxl:pl-24 text-justify">
