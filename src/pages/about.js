03b229295d3fdcccdad080d0be6e79b40c8aaea9
inesniedermaier <ines.niedermaier@gmail.com>
2025-06-25 16:27:26 +0200

    Adjust experience and education

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,15 +26,15 @@ const about = () => {
         <Layout className="pt-16">
           <AnimatedHeadline text="Das bin ich" className="text-primary" />
           {/* About Section */}
-          <section className="about-section relative max-w-[1800px] w-full mx-auto flex flex-col-reverse lg:flex-row items-center lg:items-start mt-10 mb-36 md:mb-16 lg:mb-32">
-            <div className="w-full h-full self-center lg:w-1/2 flex justify-center lg:justify-start pl-0 xl:pl-16 xxl:pl-36 pt-4 lg:pt-12">
+          <section className="about-section relative max-w-[1800px] w-full mx-auto flex flex-col-reverse lg:flex-row items-center lg:items-start mt-10 mb-36 md:mb-4 lg:mb-32">
+            <div className="w-full h-full self-center lg:w-1/2 flex justify-center lg:justify-start pl-0 xl:pl-16 xxl:pl-36 pt-12">
               <PolaroidImages />
             </div>
             <div className="w-full lg:w-1/2 px-0 lg:pl-10 xxl:pl-24 text-justify">
               <AboutText />
             </div>
           </section>
-          <section className="techStack-section relative w-full my-16 md:my-32">
+          <section className="techStack-section relative w-full py-12">
             <Skills />
           </section>
           <section className="cv-section relative max-w-[1500px] w-full mx-auto justify-center">
